b247ce8c07333d1dfc498cf2d8eae71479933af0
1845598311 <53561490+1845598311@users.noreply.github.com>
2020-03-05 15:50:22 +0800

    补码加密
    补码加密

---
+++ 补码加密.c
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 100
+char CharEncoding(char ch);
+void SentenceEncoding(char* soure, char* code);
+
+int main(){
+	int a;
+	printf("enter 1 to start,enter a new line to end!\n");
+	scanf("%d", &a);
+	while (a==1) {
+
+		a = getchar();
+		char source[N], code[N];
+		printf("Input source sentence:");
+		gets(source);//用户可能会输入空格，不能使用scanf
+		SentenceEncoding(source, code);
+		printf("Encoded sentence:");
+		puts(code);
+		printf("to continue enter 1,to end enter 0.\n");
+		
+		if (scanf("%d", &a)!=1)
+			break;
+	}
+	return 0;
+}
+void SentenceEncoding(char* source, char* code)
+{
+	int i;
+	for (i = 0; source[i] != '\0'; i++)
+	{
+		code[i] = CharEncoding(source[i]);
+	}
+	code[i] = '\0';
+}
+char CharEncoding(char ch)
+{
+	char encode = 0;
+	if (ch >= 'A' && ch <= 'Z')
+	{
+		encode = ('A' + 'Z'+1) - ch;
+	}
+	else if (ch >= 'a' && ch <= 'z')
+	{
+		encode = 'a' + 'z'+1 - ch;
+	}
+	else
+	{
+		switch (ch)
+		{
+		case ' ': encode = '@';
+			break;
+		case '.': encode = '#';
+			break;
+		default: encode = '.';
+			break;
+		}
+	}
+	return encode;
+}
+
